5197a543975b28930b5091b0db6f0e8409405069
FahimReza <FahimReza202@outlook.com>
2025-01-07 21:37:15 +0330

    Create The Reset Password and Save The Chnaged of message.py and base.html

--- core/accounts/views.py
+++ core/accounts/views.py
@@ -12,3 +12,21 @@ class UserLoginView(auth_views.LoginView):
 
 class UserLogoutView(auth_views.LogoutView):
     pass
+
+
+
+class ResetPasswordView(auth_views.PasswordResetView):
+    template_name = 'accounts/password_reset.html'
+    email_template_name = 'accounts/password_reset_email.html'
+    success_message = "We've emailed you instructions for setting your password, " \
+                      "if an account exists with the email you entered. You should receive them shortly." \
+                      " If you don't receive an email, " \
+                      "please make sure you've entered the address you registered with, and check your spam folder."
+    success_url = '/'
+
+
+
+class CustomPasswordResetCompleteView(auth_views.PasswordResetCompleteView):
+    template_name="accounts/password_reset_complete.html"
+    success_url='/accounts/login/'
+
