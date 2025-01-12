d7c5527be1c05fb5b269ae59b5071324ea1b2641
FahimReza <FahimReza202@outlook.com>
2025-01-12 22:57:52 +0330

    Craete The Product-Detail

--- core/shop/urls.py
+++ core/shop/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name="shop"
 urlpatterns = [
     path('product/grid/', views.ShopProductGridView.as_view(), name="product-grid"),
-    # re_path(r"product/(?P<slug>[\u0600-\u06FF\-a-zA-Z0-9_]+)/detail/", views.ShopProductDetailView.as_view(), name="product-detail"),
+    re_path(r"product/(?P<slug>[\u0600-\u06FF\-a-zA-Z0-9_]+)/detail/", views.ShopProductDetailView.as_view(), name="product-detail"),
 ]
